704fdbd0d5cf42d36aae0dabc131903a432826e2
Gihan S <gihanshp@gmail.com>
2020-10-20 17:54:42 +0530

    Initial commit with basic graphql server.

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongodb:
+    'mongodb+srv://merng:eIQqL8naoWQ7Gexi@cluster0.matrg.gcp.mongodb.net/merng?retryWrites=true&w=majority',
+  secretKey: 'jahshie7Pu',
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { mongodb } = require('./config');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(mongodb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => server.listen({ port: 5000 }))
+  .then((res) => {
+    console.log(`server is running at ${res.url}`);
+  });
