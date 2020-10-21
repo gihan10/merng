74982780d2de59190339f6030ed15472ee733341
Gihan S <gihanshp@gmail.com>
2020-10-21 09:35:51 +0530

    Added checkAuth utility to request authorization.

---
+++ utils/checkAuth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server');
+
+const { secretKey } = require('../config');
+
+module.exports = (context) => {
+  // extract the authorization header from request headers
+  const authHeader = context.req.headers.authorization;
+  if (!authHeader) {
+    throw new Error('Authorization header is required');
+  }
+
+  // extract the Bearer token
+  const token = authHeader.split('Bearer ')[1];
+  if (!token) {
+    throw new Error('Authentication token must be \'Bearer [token]');
+  }
+
+  // validate the token
+  try {
+    const user = jwt.verify(token, secretKey);
+    return user;
+  } catch (err) {
+    throw new AuthenticationError('Invalid/Expired token');
+  }
+};
