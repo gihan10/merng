b599e570a989f15bd609fc977df4d7df1b1cf53b
Gihan S <gihanshp@gmail.com>
2020-10-21 12:06:48 +0530

    Added CURD operations for comments.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,13 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+  }
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
   }
   type User {
     id: ID!
@@ -28,5 +35,7 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: String!): Post!
   }
 `;
